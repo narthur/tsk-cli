46dea018a38d0f6574aa049f0879d291e0ad416f
Nathan Arthur <nathan@nathanarthur.com>
2019-06-30 06:45:09 -0400

    Fix Mark cross references; add shebang; add interactivity; update readme

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 
 Treasury of Scripture Knowledge Command Line Interface
 
-It's dead simple. Running this:
+## Installation
 
-    python tsk-cli.py jo3:16
+0. `pip install -r requirements.txt`
+0. `chmod +x ./tsk-cli.py`
 
-Outputs this:
+## Usage
+
+Passage can be specified via command line arguments or interactively.
+
+- `./tsk-cli.py jo3:16`
+- `./tsk-cli.py John 3:16`
+- `./tsk-cli.py` (interactive)
+
+Output:
 
     # John 3:16
     
@@ -24,7 +33,3 @@ Outputs this:
     John 3:16
     Romans 5:8
     Romans 8:32
-
-## Dependencies
-
-- bible 0.2
--- tsk-cli.py
+++ tsk-cli.py
@@ -1,28 +1,38 @@
-import bible, sys, csv
+#!/usr/bin/env python2.7
 
-tsk = []
+import bible
+import csv
+import sys
+import re
+
+if len(sys.argv) > 1:
+    ref_args = sys.argv[1:]
+    ref_string = ' '.join(ref_args)
+else:
+    ref_string = raw_input("Reference: ")
 
 with open('tskxref.txt') as csvFile:
     csvReader = csv.DictReader(csvFile, delimiter='\t')
-    for row in csvReader:
-        tsk.append(row)
+    tsk = list(csvReader)
 
-referenceArgs = sys.argv[1:]
-referenceString = ' '.join(referenceArgs)
-reference = bible.Verse(referenceString)
+reference = bible.Verse(ref_string)
 
 print
 print '# {0}'.format(reference.format())
 print
 
 for row in tsk:
-    if int(row['book']) == reference.book \
-    and int(row['chapter']) == reference.chapter \
-    and int(row['verse']) == reference.verse:
+    matches_book = int(row['book']) == reference.book
+    matches_chapter = int(row['chapter']) == reference.chapter
+    matches_verse = int(row['verse']) == reference.verse
+
+    if matches_book and matches_chapter and matches_verse:
         print '## {0}'.format(row['words'])
         print
 
-        refs = row['refs'].split(';')
+        # bible library chokes on mr as an abbreviation for Mark
+        ref_string = re.sub(r'\bmr\b', r'Mark', row['refs'])
+        refs = ref_string.split(';')
         for ref in refs:
             reference = bible.Verse(ref)
             print reference.format()
